fbf2b5f6c7103eca93e4c50588a7d24f9c33d025
foyarekhua <fatimahoyarekhua@gmail.com>
2023-08-16 18:45:15 -0400

    added tests for cash to mile conversions

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,21 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 50;
+        var rewardValue = new RewardValue(cashValue);
+
+        int MilesValue = rewardValue.getMilesValue();
+        int expectedMilesValue = 14285;
+        assertEquals(MilesValue, expectedMilesValue);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        var rewardValue = new RewardValue(milesValue);
+
+        double cashValue = rewardValue.getCashValue();
+        double expectedCashValue = 3.5;
+        assertEquals(cashValue, expectedCashValue);
     }
 }
